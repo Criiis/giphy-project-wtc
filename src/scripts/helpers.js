2925174674dc3074f0599ddfec6de99ea55f8043
Criiis <cristiano.luis@wundermanthompson.com>
2022-09-01 21:17:58 +0100

    add file for helpers and import data mock

---
+++ src/scripts/helpers.js
@@ -0,0 +1,3 @@
+/**
+ * This file contains all the functions that will be used multiple times
+ */
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
-;(function () {
-  console.log('hello world')
-})()
+/**
+ * where the magic will happen all the action will happen
+ */
+import data from './mock.json'
+
+console.log(data)
+// console.log('hello world')
+
+//Technical requirements
+
+// You are free to implement this application in your choice of frontend language, e.g. jQuery, React, etc. the assessment is more to ascertain how you are doing in frontend so far rather than checking your ability in a certain codebase.
+// For the data required to bring your application to life, please register for a free account on giphy.com, then review the documentation available here: https://developers.giphy.com/docs/api/endpoint
+// Please use your CSS knowledge to present the application nicely and bring it to life, but we’re not looking for a professional design here, just a user engaging and friendly experience (not an unstyled application). Please use some colour!
+
+// Delivery requirements
+
+// Once you have completed your mini project and you are happy for it to be reviewed, please ensure you include a readme file (if any steps required to run it) and commit the application to your GitHub – add the link to the task on the board for review. Alternative you could also ZIP up the package and attach it to the task on the board if GitHub isn’t an option for you.
+// You should allocate 3 days to this task.
+
+// Acceptance Criteria
+
+// HTML is valid W3C standard
+// CSS class names use BEM
+// The CSS is mobile first and responsive
+// There is validation for a blank search
+// Loading time is considered
+// Error handling is considered
+// Read me file on steps to run
+// Steps required to run the project locally
+
+
