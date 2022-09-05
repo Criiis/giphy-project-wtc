877cf689f85736df6ae232f2cd9e7ab19e44ac8c
Criiis <cristiano.luis@wundermanthompson.com>
2022-09-05 22:02:53 +0100

    remove code from main file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,12 @@ import {
   getJSON,
   gifLoading,
   imageLoadChecker,
+  promiseAllImage,
 } from './helpers.js'
 import randomView from './views/randomView.js'
-import { gifStructure } from './views/viewHelpers.js'
+import searchView from './views/searchView.js'
+import { gifStructure, singleGifContainer } from './views/viewHelpers.js'
 // import data from './mock.json'
-
 
 //Technical requirements
 
@@ -34,77 +35,10 @@ import { gifStructure } from './views/viewHelpers.js'
 // Error handling is considered
 // Read me file on steps to run
 // Steps required to run the project locally
-
-
-//for search gif
-const searchRandom = async function (querySearch) {
-  try {
-    // get API data
-    const data = await getJSON(`${API_SEARCH_GIF}&q=${querySearch}`)
-    console.log(data)
-    if (data.length === 0)
-      throw new Error('Could not find any results, try again.')
-
-    data.forEach(
-      ({ title, images }) =>
-        document.querySelector('.search-section--results').insertAdjacentHTML(
-          'beforeend',
-          `
-          <div class="search-section--picture-container">
-        <picture class="">
-          <source type="image/webp" srcset="${images.preview_webp.url}" />
-          <img src="${images.preview_gif.url}" alt="${title}" loading="lazy" />
-        </picture>
-        </div>
-        `
-        ) //getting the smallest image possible instead of the big ones
-    )
-
-    for (
-      let i = 0;
-      i <
-      document.querySelectorAll('.search-section--picture-container').length;
-      i++
-    ) {
-      const container = document.querySelectorAll(
-        '.search-section--picture-container'
-      )[i]
-      console.log(container)
-      // console.log(container.querySelectorAll('picture *'))
-
-      Promise.all(
-        Array.from(container.querySelectorAll('picture *')).map(async image => {
-          // get response from image loader checker function
-          await imageLoadChecker(image)
-          // add class loaded if the promise fulfilled
-          image
-            .closest('.search-section--picture-container')
-            .classList.add('loaded')
-        })
-      )
-    }
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-
 ;(function () {
   randomView.controlRandom() // initialize the random gif
   randomView.reloadHandler() //add click event for "next" in random section
 
   //add search functionality
-  document
-    .querySelector('.search-section .search-section--form .btn')
-    .addEventListener('click', function (e) {
-      e.preventDefault()
-      const searchValue = document.querySelector(
-        '.search-section--form input#search'
-      )
-
-      document.querySelector('.search-section--results').innerHTML = ''
-      searchRandom(searchValue.value)
-      console.log(searchValue)
-      searchValue.value = ''
-    })
+  searchView.searchHandler()
 })()
