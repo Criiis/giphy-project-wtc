7838fb7d1fd4b965e9dff28e551e2ee0bd535477
Criiis <cristiano.luis@wundermanthompson.com>
2022-09-05 22:02:19 +0100

    fixing some small things in the random gif view

--- src/scripts/views/randomView.js
+++ src/scripts/views/randomView.js
@@ -3,23 +3,19 @@ import {
   errorLoading,
   getJSON,
   gifLoading,
-  imageLoadChecker,
+  promiseAllImage,
 } from '../helpers.js'
 
 class RandomView {
   _parentElement = document.querySelector('section.random-section')
   imageParent = '.random-section--picture-container'
-
   // picture container -> this will be very important element as it is the one with loading screen
   pictureParentSection = () =>
     this._parentElement.querySelector(this.imageParent)
-
   // picture element
   pictureSection = () => this.pictureParentSection().querySelector('picture')
-
   // all elements inside the picture
   pictureSectionElements = () => this.pictureSection().querySelectorAll('*')
-
   // shuffle button
   _shuffleCTA = this._parentElement.querySelector(
     '.random-section--shuffle-gif'
@@ -35,31 +31,29 @@ class RandomView {
 
   //reset section for random gif
   _resetSection() {
-    this.pictureParentSection().classList.remove('loaded')
-    this.pictureSection().remove()
+    this.pictureParentSection()?.classList.remove('loaded')
+    this.pictureSection()?.remove()
   }
 
-  //API call for this view
+  //API Fetch call for this view
   controlRandom = async function () {
     try {
       // get API data
       const data = await getJSON(API_RANDOM_GIF)
       // add gif structure into the page
       gifLoading(this.pictureParentSection(), data)
-      // create a promise for the image to remove the loading screen only after the image is loaded
-      await Promise.all(
-        Array.from(this.pictureSectionElements()).map(async image => {
-          // get response from image loader checker function
-          await imageLoadChecker(image)
-          // add class loaded if the promise fulfilled
-          image.closest(this.imageParent).classList.add('loaded')
-        })
-      )
+
+      await promiseAllImage(this.pictureSectionElements(), this.imageParent)
     } catch (err) {
+      //IMPORTANT
+      //:TODO CAN BE IMPROVED TO A SINGLE FUNCTION
+      //
+
       //return the error, err can be the image that didn't load from the promise all
+      console.error(err)
       if (err.nodeType)
-        return errorLoading(err, this.imageParent, `Image not found`)
-      errorLoading(this.pictureParentSection(), this.imageParent, err.message)
+        return errorLoading(err, `Image not found`, this.imageParent)
+      errorLoading(this.pictureParentSection(), err.message)
     }
   }
 }
