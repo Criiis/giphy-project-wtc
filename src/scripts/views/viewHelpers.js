b846e522958eee7ef559a5c0035725f3878de27e
Criiis <cristiano.luis@wundermanthompson.com>
2022-09-06 12:48:15 +0100

    fixing bug within picture

--- src/scripts/views/viewHelpers.js
+++ src/scripts/views/viewHelpers.js
@@ -1,6 +1,6 @@
 export const gifStructure = ({ title, images }) => `
-<picture class="random-section--picture">
-    <source type="image/webp" media="(max-width: 728px)" srcset="${images.preview_webp.url}}" /> 
+<picture>
+    <source type="image/webp" media="(max-width: 728px)" srcset="${images.preview_webp.url}" /> 
     <source type="image/webp" srcset="${images.original.webp}" />
     <source media="(max-width: 728px)" srcset="${images.preview_webp.url}" />
     <img src="${images.original.url}" alt="${title}" loading="lazy" />
@@ -10,4 +10,6 @@ export const gifStructure = ({ title, images }) => `
 export const gifError = message => `<p>Sorry, ${message}. Try again.</p>`
 
 export const singleGifContainer = data => `
-<div class="search-section--picture-container">${gifStructure(data)}</div>`
+<div class="search-gif-container__picture-container">${gifStructure(
+  data
+)}</div>`
