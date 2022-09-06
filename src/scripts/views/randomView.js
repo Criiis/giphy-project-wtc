44eee71f96dae31bc553543d659761dd0cfce1be
Criiis <cristiano.luis@wundermanthompson.com>
2022-09-06 12:47:47 +0100

    reorganizing random section view

--- src/scripts/views/randomView.js
+++ src/scripts/views/randomView.js
@@ -7,33 +7,20 @@ import {
 } from '../helpers.js'
 
 class RandomView {
-  _parentElement = document.querySelector('section.random-section')
-  imageParent = '.random-section--picture-container'
+  _sectionName = 'random-gif-container'
+  _imageParent = `.${this._sectionName}__picture-container`
+  _parentElement = document.querySelector(`section.${this._sectionName}`)
   // picture container -> this will be very important element as it is the one with loading screen
-  pictureParentSection = () =>
-    this._parentElement.querySelector(this.imageParent)
+  _pictureParentSection = () =>
+    this._parentElement.querySelector(this._imageParent)
   // picture element
-  pictureSection = () => this.pictureParentSection().querySelector('picture')
+  _pictureSection = () => this._pictureParentSection().querySelector('picture')
   // all elements inside the picture
-  pictureSectionElements = () => this.pictureSection().querySelectorAll('*')
+  _pictureSectionElements = () => this._pictureSection().querySelectorAll('*')
   // shuffle button
   _shuffleCTA = this._parentElement.querySelector(
-    '.random-section--shuffle-gif'
+    `.${this._sectionName}__shuffle-gif`
   )
-
-  //reload functionality for CTA
-  reloadHandler() {
-    this._shuffleCTA.addEventListener('click', () => {
-      this._resetSection() //bind(this) wasn't working with a traditional function not sure why, so transformed this into arrow function :$
-      this.controlRandom()
-    })
-  }
-
-  //reset section for random gif
-  _resetSection() {
-    this.pictureParentSection()?.classList.remove('loaded')
-    this.pictureSection()?.remove()
-  }
 
   //API Fetch call for this view
   controlRandom = async function () {
@@ -41,20 +28,34 @@ class RandomView {
       // get API data
       const data = await getJSON(API_RANDOM_GIF)
       // add gif structure into the page
-      gifLoading(this.pictureParentSection(), data)
-
-      await promiseAllImage(this.pictureSectionElements(), this.imageParent)
+      gifLoading(this._pictureParentSection(), data)
+      // await and remove the loading screen when gif is loaded
+      await promiseAllImage(this._pictureSectionElements(), this._imageParent)
     } catch (err) {
-      //IMPORTANT
-      //:TODO CAN BE IMPROVED TO A SINGLE FUNCTION
-      //
-
-      //return the error, err can be the image that didn't load from the promise all
-      console.error(err)
-      if (err.nodeType)
-        return errorLoading(err, `Image not found`, this.imageParent)
-      errorLoading(this.pictureParentSection(), err.message)
+      this._errorHandler(err)
     }
+  }
+
+  //handel the error for the async function
+  _errorHandler(err) {
+    console.error(err)
+    if (err.nodeType)
+      return errorLoading(err, `Image not found`, this._imageParent)
+    errorLoading(this._pictureParentSection(), err.message)
+  }
+
+  //reset section for random gif
+  _resetSection() {
+    this._pictureParentSection()?.classList.remove('loaded', 'error')
+    this._pictureSection()?.remove()
+  }
+
+  //reload functionality for CTA
+  reloadHandler() {
+    this._shuffleCTA.addEventListener('click', () => {
+      this._resetSection() //bind(this) wasn't working with a traditional function, not sure why, so transformed this into arrow function and this keyword is related to the parent
+      this.controlRandom()
+    })
   }
 }
 
